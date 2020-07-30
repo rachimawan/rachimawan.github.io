15b83f6efc5a04ebdbb51e9a692f267354889ccc
Fariz Rachimawan <rachimawan@gmail.com>
2020-07-30 22:39:41 +0700

    tugas web sederhana dengan DOM

---
+++ script.js
@@ -0,0 +1,34 @@
+// Menghitung Zakaat
+function calculateZakaat() {
+    let nomAmt = document.getElementById("nomamt").value
+
+      //Jika user menginput syarat hanya 1 atau tidak menginput sama sekali
+      let penghasilan = document.getElementById("penghasilan").checked
+      let syarat = document.getElementById("syarat").checked
+      
+      if (penghasilan === false || syarat === false) {
+          alert('Tolong dipenuhi dulu syarat-syaratnya ya!')
+          return
+      }
+    
+    
+    //Jika user memasukan input besar nominal yang kosong atau string
+    if (nomAmt === '' || nomAmt === 0 || nomAmt === undefined || !nomAmt || Number.isNaN(Number(nomAmt))) {
+        alert('Masukan nominal angka!')
+        return
+    }
+    
+  
+    
+    //Menghitung berapakah zakatmu?
+    let zakaat = (nomAmt * 2.5/100)
+    document.getElementById("totalZakaat").style.display = "block";
+    document.getElementById("total").innerHTML = zakaat;
+}
+
+
+// Jika hitung zakaat diclick, maka eksekusi function calculateZakaat
+document.getElementById("calculate").onclick = function() {
+    calculateZakaat()
+  }
+
